302defe6a00d34484598a60baa399d24dcf4a18d
NestaKobe <simon.coburg@gmail.com>
2021-01-07 19:21:49 +0100

    Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates a special "matrix" object that can cache its inverse
 
+makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
+    inv <- NULL             ## initialize inv as NULL; will hold value of matrix inverse 
+    set <- function(y) {    ## define the set function to assign new 
+      x <<- y               ## value of matrix in parent environment
+      inv <<- NULL          ## if there is a new matrix, reset inv to NULL
+    }
+    get <- function() x     ## define the get function - returns value of the matrix argument
+    setInverse <- function(inverse) inv <<- inverse ## assigns value of inv in parent environment 
+    getInverse <- function() inv                    ## gets the value of inv where called
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) 
+    ## you need this in order to refer to the functions with the $ operator
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+
+
+example <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+
+example$get()
+  #     [,1] [,2]
+  #[1,]    1    3
+  #[2,]    2    4
+
+example$getInverse()
+  #NULL
+
+cacheSolve(example)
+  #     [,1] [,2]
+  #[1,]   -2  1.5
+  #[2,]    1 -0.5
+
+
+example$getInverse()
+  #     [,1] [,2]
+  #[1,]   -2  1.5
+  #[2,]    1 -0.5
